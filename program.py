8c8ffe5e9071b25acb80dcfac836c6a588007b2e
cj6 <165647634+Cursajoz@users.noreply.github.com>
2026-03-28 12:19:48 +0000

    initial commit

---
+++ program.py
@@ -0,0 +1,32 @@
+from datetime import datetime
+from dotenv import load_dotenv
+from time import time
+import os
+import urllib3
+import json
+
+load_dotenv()
+
+headers = {
+    "Cookie" : os.getenv("COOKIE")
+}
+
+model_id = int(input("Enter Model id: "))
+final_version = int(input("Enter version you will go up to: "))
+
+for version in range(1, final_version + 1):
+    u = f"https://assetdelivery.roblox.com/v2/assetId/{model_id}/version/{version}"
+    r_data = urllib3.request("GET", url=u, headers=headers)
+    decoded_data = r_data.data.decode("utf8")
+    json_coded = json.loads(decoded_data)
+    version_url = json.dumps(json_coded["locations"][0]["location"])
+
+    v_data = urllib3.request("GET", url=version_url.replace('"', ""))
+    date_str = v_data.info().getheaders("Last-Modified")[0]
+    
+    stripped_time = datetime.strptime(date_str, "%a, %d %b %Y %H:%M:%S %Z")
+    unix_stamp = stripped_time.timestamp()
+    with open(f"output/{model_id}-v{version}.rbxm", "wb") as f:
+        f.write(v_data.data)
+    os.utime(f"output/{model_id}-v{version}.rbxm", (time(), unix_stamp))
+    
