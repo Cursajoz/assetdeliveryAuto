7350c797fb800c6c9b045a860936a9404fea42dd
cj6 <165647634+Cursajoz@users.noreply.github.com>
2026-04-28 22:01:14 +0100

    added support for rbxl files

--- program.py
+++ program.py
@@ -13,11 +13,23 @@ headers = {
 
 model_id = int(input("Enter Model id: "))
 final_version = int(input("Enter version you will go up to: "))
+# rbxm or rbxl
+
+print("TYPE 'M' for model or type 'L' for place")
+
+filetype_extension = input()
+
+filetype_extension = filetype_extension.lower()
+
+filetype_validation = (filetype_extension is None) or (filetype_extension != "l" and filetype_extension != "m") or len(filetype_extension) > 1
+
+if filetype_validation:
+    filetype_extension = "m"
 
 for version in range(1, final_version + 1):
     u = f"https://assetdelivery.roblox.com/v2/assetId/{model_id}/version/{version}"
     r_data = urllib3.request("GET", url=u, headers=headers)
-    decoded_data = r_data.data.decode("utf8")
+    decoded_data = r_data.data.decode("utf-8")
     json_coded = json.loads(decoded_data)
     version_url = json.dumps(json_coded["locations"][0]["location"])
 
@@ -26,7 +38,7 @@ for version in range(1, final_version + 1):
     
     stripped_time = datetime.strptime(date_str, "%a, %d %b %Y %H:%M:%S %Z")
     unix_stamp = stripped_time.timestamp()
-    with open(f"output/{model_id}-v{version}.rbxm", "wb") as f:
+    with open(f"output/{model_id}-v{version}.rbx{filetype_extension}", "wb") as f:
         f.write(v_data.data)
-    os.utime(f"output/{model_id}-v{version}.rbxm", (time(), unix_stamp))
+    os.utime(f"output/{model_id}-v{version}.rbx{filetype_extension}", (time(), unix_stamp))
     
